95ed9695417db95d7d2943216b6995a60fe26749
Rubens Machado <rubensclmachado@gmail.com>
2019-07-06 16:51:05 -0300

    Criação de modules

--- src/app/agenda/agenda-routing.module.ts
+++ src/app/agenda/agenda-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AgendaListComponent} from './containers/agenda-list/agenda-list.component';
+import {AgendaDetailComponent} from './containers/agenda-detail/agenda-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'list'},
+  {path: 'list', component: AgendaListComponent},
+  {path: 'detail', component: AgendaDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/agenda/agenda.module.ts
+++ src/app/agenda/agenda.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AgendaRoutingModule } from './agenda-routing.module';
+import { AgendaListComponent } from './containers/agenda-list/agenda-list.component';
+import { AgendaDetailComponent } from './containers/agenda-detail/agenda-detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AgendaListComponent, AgendaDetailComponent],
   imports: [
     CommonModule,
     AgendaRoutingModule
